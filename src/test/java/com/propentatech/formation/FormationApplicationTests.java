90e06f953c478d6432d8144ad73d169fbf7575b6
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2025-12-16 13:05:14 +0100

    Création du projet

---
+++ src/test/java/com/propentatech/formation/FormationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propentatech.formation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
